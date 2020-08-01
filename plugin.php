5a2aff6ff7537e3fdc818a4c400ad8eb3f55f475
Steven Roland <steven@mojave.io>
2020-08-01 16:53:41 +0000

    Apply fixes from StyleCI

--- plugin.php
+++ plugin.php
@@ -7,47 +7,48 @@
  * Author URI: https://mojavehq.com/
  * Version: 0.0.1
  * License: GPL2+
- * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt.
  */
 
 namespace MojaveHQ\Bricks;
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 function register()
 {
     \wp_register_style(
-		'bricks-style',
-		plugin_dir_url(__FILE__) . 'public/css/plugin.css',
-		is_admin() ? array('wp-editor') : null,
-		null
+        'bricks-style',
+        plugin_dir_url(__FILE__).'public/css/plugin.css',
+        is_admin() ? ['wp-editor'] : null,
+        null
     );
-    
+
     \wp_register_style(
-		'bricks-editor-style',
-		plugin_dir_url(__FILE__) . 'public/css/plugin.css',
-		array('wp-edit-blocks'),
-		null
+        'bricks-editor-style',
+        plugin_dir_url(__FILE__).'public/css/plugin.css',
+        ['wp-edit-blocks'],
+        null
     );
-    
+
     \wp_register_script(
-		'bricks-script',
-		plugin_dir_url(__FILE__) . 'public/js/plugin.js',
-		array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'),
-		null,
-		true
+        'bricks-script',
+        plugin_dir_url(__FILE__).'public/js/plugin.js',
+        ['wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'],
+        null,
+        true
     );
-    
+
     \register_block_type(
-		'bricks/block', array(
-			'style'         => 'bricks-css',
-			'editor_script' => 'bricks-js',
-			'editor_style'  => 'bricks-editor-css',
-		)
-	);
+        'bricks/block',
+        [
+            'style'         => 'bricks-css',
+            'editor_script' => 'bricks-js',
+            'editor_style'  => 'bricks-editor-css',
+        ]
+    );
 }
 
-\add_action('init', __NAMESPACE__ . '\\register');
+\add_action('init', __NAMESPACE__.'\\register');
